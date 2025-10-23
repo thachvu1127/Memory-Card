eeab13730ee86ad00309cf3c611851cc29ce6b32
Thach Vu <Thachvu1127@gmail.com>
2025-10-23 16:37:11 -0700

    add background image and start on decorating homepage button

---
+++ Memory-Card-App/src/components/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import pokeball from "../../public/pokeball.jpg";
+import HomePageButton from "./HomePageButton";
+
+function HomePage() {
+  return (
+    <div
+      className="min-h-screen bg-cover bg-center flex items-center justify-center"
+      style={{ backgroundImage: `url(${pokeball})` }}
+    >
+      <div className="absolute inset-0 bg-black/20"></div>
+      <HomePageButton></HomePageButton>
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ Memory-Card-App/src/components/HomePageButton.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HomePageButton() {
+  return (
+    <div className="max-h-full w-md md:w-3xl lg:w-4xl border border-amber-600 flex flex-col justify-center items-center p-10 bg-white/20 rounded-full">
+      <h2>Test your memory</h2>
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default HomePageButton;
